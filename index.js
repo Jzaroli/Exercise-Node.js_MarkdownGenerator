6e2e693580d791b49961fc7b1d6171fee779eb84
Jzaroli <jzaroli@gmail.com>
2024-10-20 16:23:26 -0600

    completed main functionality requirements

--- index.js
+++ index.js
@@ -1,15 +1,77 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
 import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown.js";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's title?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please briefly describe your project?",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are your project's installation instructions?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is your project's usage information?",
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "How would a user contribute to your project? if applicable.",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be used to test the application?",
+    },
+    {
+        type: "list",
+        message: "Which license will you be using?",
+        name: "license",
+        choices: ['MIT', 'Apache', 'GPL', 'Mozilla', 'Other'],
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's your email address so people can reach out to you?",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What's your GitHub username so people can see your work?",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//     const data = generateMarkdown(data)
+//     fs.writeFile('README.md', process.argv[2], (err) =>
+//         err ? console.error(err) : console.log('Success!')
+//       );
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+    const ReadMeContent = generateMarkdown(answers);
+
+    fs.writeFile('README.md', ReadMeContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created READme.md!')
+    );
+  });
+
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge() {
+ if (data.license === MIT) {
+    return '![MIT](./images/MIT.jpg)'
+ } else if (data.license === Apache) {
+    return '![Apache](./images/apache.jpg)'
+ } else if (data.license === GPL) {
+    return '![GPL](./images/GPL.jpg)'
+ } else if (data.license === Mozilla) {
+    return '![Mozilla](./images/MIT.jpg)'
+ } else if (data.license === Other) {
+    '';
+ }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,6 +26,37 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+
+1. [Installation](#installation)  
+
+2. [Usage](#usage)  
+
+3. [Contributing](#contributing)  
+
+4. [Tests](#tests)  
+
+5. [Questions](#questions)  
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+To test the application, run ${data.test}
+
+## Questions
+If you have any questions, you can reach out to me at ${data.email} or find me on GitHub as ${data.github}.
 `;
 }
 
